fd6676217e9623ce3341912948bc1edc6e8f0b75
GurungRegina <gina524286@gmail.com>
2019-01-21 22:45:59 +0000

    delete email stuff

--- btre/settings.py
+++ btre/settings.py
@@ -136,15 +136,8 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email Configs
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ginagrg@gmail.com'
-EMAIL_HOST_PASSWORD = 'something1234'
-EMAIL_USE_TLS=True
 
 try:
     from .local_settings import *
 except ImportError:
     pass
-    
